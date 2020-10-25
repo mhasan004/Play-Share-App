f0c5784fc29cbbfbe74be081239efd4df1134e2f
Mahmudul Hasan <mhasan0047@gmail.com>
2020-10-25 18:29:53 -0400

    changing up post ui

--- CLIENT_REACT/play-share-app/src/JSX/Components/Post.jsx
+++ CLIENT_REACT/play-share-app/src/JSX/Components/Post.jsx
@@ -55,8 +55,8 @@ class Post extends React.Component{
                         <svg class="svg-stroke-container"  width="319" height="69" viewBox="0 0 319 69" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M94.5241 5.58364C51.6238 10.4037 29.9999 -3 3.49995 26C-6 46 7.5 59 7.5 59C7.5 59 16.9999 67 29.9999 67C88.8501 67 193.072 43.0005 244.464 63.321C285.027 79.3601 306.476 56.3305 306.476 56.3305C314.725 49.7491 329.965 29.9013 306.476 19.6364C244.16 -7.59607 148.126 -0.438726 94.5241 5.58364Z" fill="#FF4B2B"/>
                         </svg>
-                        <i class="post-top-handle"> {this.state.handle}:{this.props.user} </i>
-                        <i class="post-top-date"> {this.state.date} </i>
+                        <h1 class="post-top-handle"> {this.state.handle} </h1>
+                        <h1 class="post-top-date"> {this.state.date} </h1>
                     </div>  
 
                     <div class="post-title-content-container"> 
@@ -70,26 +70,36 @@ class Post extends React.Component{
 
                     </div> 
                 </div>
-                
-                
-{/* 
-                <span class="post-arrows">
-                    <a class="post-arrow-up" onClick={e=>this.like(e)}>      <i class="fas fa-arrow-up"></i></a>
-                    <a class="post-total-likes-text">   {this.state.total_likes}  </a>
-                    <a class="post-arrow-down" onClick={e=>this.dislike(e)}> <i class="fas fa-arrow-down"></i></a>
-                </span>
 
+                <div class="post-actions-constainer ">
+                    <span class="post-actions-left">
+                        <svg class="post-arrow-up post-arrows" onClick={e=>this.like(e)} viewBox="0 0 44 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M22 0L44 26L0 26L22 0Z" fill="#2BB3FF"/>
+                            <path d="M11.7759 26.0033L32.7759 26.0033V52.0033H11.7759V26.0033Z" fill="#2BB3FF"/>
+                        </svg>
+                        <a class="post-total-likes-text">   {this.state.total_likes}  </a>
+                        <svg class="post-arrow-down post-arrows" onClick={e=>this.dislike(e)} viewBox="0 0 44 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M22 52.0033L0 26.0033H44L22 52.0033Z" fill="#FF4B2B"/>
+                            <path d="M32.2241 26H11.2241V0H32.2241V26Z" fill="#FF4B2B"/>
+                        </svg>
+                    </span>
 
-                <span class="post-actions">
                     <a class="post-comment" onClick={e=>this.comment(e)}>  <i class="far fa-comment-dots"></i></a>
-                    <a class="post-edit" onClick={e=>this.edit(e)}>     <i class="fas fa-pen"></i></a>
-                    <a class="post-delete" onClick={e=>this.delete(e)}> <i class="far fa-trash-alt"></i></a>
-                </span> */}
 
-
+                    <span class="post-actions-right">
+                        <a class="post-edit" onClick={e=>this.edit(e)}>     <i class="fas fa-pen"></i></a>
+                        <a class="post-delete" onClick={e=>this.delete(e)}> <i class="far fa-trash-alt"></i></a>
+                    </span> 
+                </div>
             </div>
         );
     }
 }
 
 export default Post;
+
+
+{/* <a class="post-arrow-up post-arrows" onClick={e=>this.like(e)}>      <i class="fas fa-arrow-up"></i></a> */}
+
+{/* <a class="post-total-likes-text">   {this.state.total_likes}  </a>
+<a class="post-arrow-down post-arrows" onClick={e=>this.dislike(e)}> <i class="fas fa-arrow-down"></i></a> */}
